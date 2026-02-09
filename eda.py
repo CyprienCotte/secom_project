79e5693c77804d4732910a57423c68260ea18391
CyprienCotte <cypriencotte27@gmail.com>
2026-02-09 19:05:06 +0100

    Data Loading

--- eda.py
+++ eda.py
@@ -1,1 +1,21 @@
-print("hello, this is my first test with micro")
+# print("hello, this is my first test with micro")
+
+import pandas as pd 
+
+def load_data():
+
+    X = pd.read_csv('secom.data', sep = '\s+', header = None)
+
+    y = pd.read_csv('secom_labels.data', sep = '\s+', header = None, names = ['Result', 'Timestamp'])
+
+    print("Structure de X :", X.shape)
+    print("Structure de y :", y.shape)
+
+    # Vérification du déséquilibre des classes
+    print("\nDistribution des classes :")
+    print(y['Result'].value_counts())
+
+    return X, y 
+
+if __name__ == "__main__" :     
+    X, y = load_data()
