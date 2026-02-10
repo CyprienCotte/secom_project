12468b58cfa083b74a910bf1b6f142404a2b9043
CyprienCotte <cypriencotte27@gmail.com>
2026-02-10 21:44:30 +0100

    Split: Implémentation du Stratified Sampling pour gérer l'imbalance

--- eda.py
+++ eda.py
@@ -8,14 +8,25 @@ def load_data():
 
     y = pd.read_csv('secom_labels.data', sep = '\s+', header = None, names = ['Result', 'Timestamp'])
 
-    print("Structure de X :", X.shape)
-    print("Structure de y :", y.shape)
+    # print("Structure de X :", X.shape)
+    # print("Structure de y :", y.shape)
 
-    # Vérification du déséquilibre des classes
-    print("\nDistribution des classes :")
-    print(y['Result'].value_counts())
+    # # Vérification du déséquilibre des classes
+    # print("\nDistribution des classes :")
+    # print(y['Result'].value_counts())
 
     return X, y 
 
 if __name__ == "__main__" :     
     X, y = load_data()
+
+y_encoded = y['Result'].replace(-1, 0)
+
+# Split du dataset en train et test en utilisant le Stratified Sampling
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded)
+
+print(f"Ratio d'échecs dans train : {y_train.mean():.2%}")
+print(f"Ratio d'échecs dans test : {y_test.mean():.2%}")
