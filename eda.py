ff7a15f58cfba01c5f2c89f2d9c38ff0798e2f84
CyprienCotte <cypriencotte27@gmail.com>
2026-02-10 21:53:27 +0100

    Data Prep: Implémentation du pipeline avec Imputer et StandardScaler

--- eda.py
+++ eda.py
@@ -28,5 +28,28 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded)
 
-print(f"Ratio d'échecs dans train : {y_train.mean():.2%}")
-print(f"Ratio d'échecs dans test : {y_test.mean():.2%}")
+# print(f"Ratio d'échecs dans train : {y_train.mean():.2%}")
+# print(f"Ratio d'échecs dans test : {y_test.mean():.2%}")
+
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+
+# 1. Imputation : on remplace les NaNs par la médiane (robuste aux outliers)
+# 2. Standardisation : on applique (X - mu) / sigma 
+preprocessor = make_pipeline(
+    SimpleImputer(strategy='median'), 
+    StandardScaler()
+)
+
+# Application sur le jeu d'entraînement
+# On "fit" (apprend la médiane et l'écart-type) ET on transforme
+X_train_prepared = preprocessor.fit_transform(X_train)
+
+# Application sur le jeu de test
+# On utilise uniquement .transform() pour ne pas "fuiter" d'infos du test
+X_test_prepared = preprocessor.transform(X_test)
+
+print(f"Données d'entraînement préparées : {X_train_prepared.shape}")
+print(f"Données de test préparées : {X_test_prepared.shape}")
